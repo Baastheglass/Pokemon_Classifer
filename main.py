ddf5248e66d75274f46559ce1094434f6def9c1b
Baastheglass <lightningblazer123@gmail.com>
2025-05-12 12:57:56 +0500

    readme created and csv loaded and encoder created

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+#initialisation
+csv_data = pd.read_csv('pokemon.csv')
+encoder = LabelEncoder()
+print(csv_data)
+
+#sanitising dataset
+csv_data = csv_data.dropna()
---
+++ readme.md
@@ -0,0 +1,1 @@
+Dataset Used: https://www.kaggle.com/datasets/vishalsubbiah/pokemon-images-and-types?resource=download
